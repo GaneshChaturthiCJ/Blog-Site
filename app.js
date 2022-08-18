6bd7238f3c7c6a53e61e926b3a7a17bcfa345ed8
Ganesh Chaturthi CJ <ganeshchaturthicj@gmail.com>
2022-08-18 09:11:23 +0530

    PORT changes

--- app.js
+++ app.js
@@ -91,10 +91,10 @@ app.get("/posts/:postName", function(req, res) {
 
 
 
-//let port = process.env.PORT;
-//if (port == null || port == "") {
+let port = process.env.PORT;
+if (port == null || port == "") {
   let port = 3000;
-//}
+}
 //app.listen(port);
 
 
