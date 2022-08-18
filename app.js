b9b78ed24c4dc51c5b31a597741a2928b3004b1d
Ganesh Chaturthi CJ <ganeshchaturthicj@gmail.com>
2022-08-18 18:24:52 +0530

    Added How to use website

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent1 = "Twenty-five years ago that little robot, a six-wheeled rover named Sojourner, made it — becoming the first in a string of rovers built and operated by NASA to explore Mars. Four more NASA rovers, each more capable and complex than the last, have surveyed the Red Planet. The one named Curiosity marked its 10th year of cruising around on August 5. Another, named Perseverance, is busy collecting rocks that future robots are supposed to retrieve and bring back to Earth. China recently got into the Mars exploring game, landing its own rover, Zhurong, last year.";
+const homeStartingContent2 = "Other Mars spacecraft have done amazing science from a standstill, such as the twin Viking landers in the 1970s that were the first to photograph the Martian surface up close and the InSight probe that has been listening for Marsquakes shaking the planet’s innards (SN Online: 2/24/20). But the ability to rove turns a robot into an interplanetary field geologist, able to explore the landscape and piece together clues to its history. Mobility, says Kirsten Siebach, a planetary scientist at Rice University in Houston, “makes it a journey of discovery.";
+
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -26,7 +28,8 @@ app.get("/", function(req, res) {
 
 
   res.render("home", {
-    startingContent: homeStartingContent,
+    startingContent1: homeStartingContent1,
+    StartingContent2: homeStartingContent2,
     posts: posts
   });
 
@@ -91,13 +94,13 @@ app.get("/posts/:postName", function(req, res) {
 
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  let port = 3000;
-}
+ let port = process.env.PORT;
+ if (port == null || port == "") {
+   let port = 3000;
+ }
 //app.listen(port);
 
 
 app.listen(port, function(){
-  console.log("Server started successfully");
+  console.log("Server is up and running successfully");
 });
